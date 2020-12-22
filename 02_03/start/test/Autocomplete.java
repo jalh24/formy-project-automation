f53f3e0cceea5bfb4931ed2e4d6d1f0d25d25e1b
Adolfo Lozano <jalh24@hotmail.com>
2020-12-22 12:39:47 -0600

    Initial commit

---
+++ 02_03/start/test/Autocomplete.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\jalh2\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("8325 Peekskill Ln, Houston, TX");
+        Thread.sleep(1000);
+
+        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        autocompleteResult.click();
+
+        driver.quit();
+    }
+}
